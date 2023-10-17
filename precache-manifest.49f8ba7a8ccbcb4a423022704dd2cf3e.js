0ef0bd7dbb027f9ea170085d9660fcc84ca069b3
Amr hany El mahdy <amrhany1214@gmail.com>
2023-10-17 13:53:06 +0300

    Updates

---
+++ precache-manifest.49f8ba7a8ccbcb4a423022704dd2cf3e.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70934494a36ba3066107e7bb8dd327e2",
+    "url": "/Emojipedia/index.html"
+  },
+  {
+    "revision": "f651238ca557a19b3f75",
+    "url": "/Emojipedia/static/js/2.e8e7372e.chunk.js"
+  },
+  {
+    "revision": "e8328694a3455bd2ac8a",
+    "url": "/Emojipedia/static/js/main.0d793472.chunk.js"
+  },
+  {
+    "revision": "37105f7fe701a3739caf",
+    "url": "/Emojipedia/static/js/runtime~main.ef7b50c5.js"
+  }
+]);
